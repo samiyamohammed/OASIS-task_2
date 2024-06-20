66f26c313ed3487ceabcf61d34a0e2f851523662
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-06-20 21:50:56 +0300

    Add files via upload

---
+++ tribjs.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const container = document.querySelector('.container');
+//     const zoomIcon = document.querySelector('.zoom-icon i');
+//     const zoomIconContainer = document.querySelector('.zoom-icon');
+
+//     zoomIconContainer.addEventListener('click', function () {
+//         if (container.classList.contains('zoomed')) {
+//             container.classList.remove('zoomed');
+//             zoomIcon.classList.remove('fa-search-minus');
+//             zoomIcon.classList.add('fa-search-plus');
+//             document.body.style.overflow = 'Scroll';
+//         } else {
+//             container.classList.add('zoomed');
+//             zoomIcon.classList.remove('fa-search-plus');
+//             zoomIcon.classList.add('fa-search-minus');
+//             document.body.style.overflow = 'auto';
+//         }
+//     });
+// });
